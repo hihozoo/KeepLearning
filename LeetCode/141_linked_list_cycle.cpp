009fa6cfa039530fddb204eb7738230769f1e805
zhujisheng <zhujisheng@corp.netease.com>
2021-12-28 15:50:10 +0800

    141题，判断链表是否有环

---
+++ LeetCode/141_linked_list_cycle.cpp
@@ -0,0 +1,66 @@
+/**
+ *
+ *
+给你一个链表的头节点 head ，判断链表中是否有环。
+
+如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。
+
+如果链表中存在环，则返回 true 。 否则，返回 false 。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/linked-list-cycle
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include<unordered_set>
+
+struct ListNode
+{
+	int val;
+	ListNode* next;
+};
+
+
+class Solution {
+public:
+	/**
+	 * 快慢指针，控件复杂度是 O(1)
+	 * 如何怎么快慢指针一定会相遇呢？
+	*/
+    bool hasCycle1(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* quick = head;
+
+		while (quick != nullptr && quick->next != nullptr){
+			slow = slow->next;
+			quick = quick->next->next;
+			// 相遇了
+			if (slow == quick){
+				return true;
+			}
+		}
+		return true;
+    }
+
+	/**
+	 * 哈希表, 遍历过的都加到 set 中，如果发现在 set 中就代表曾经走过这个节点，有环
+	 * 哈希表比红黑树的插入查找时间复杂度更低
+	*/
+	bool hasCycle2(ListNode *head){
+		std::unordered_set<ListNode*> seen;
+		while(head != nullptr){
+			if (seen.count(head)){
+				return true;
+			}
+			seen.insert(head);
+			head = head->next;
+		}
+		return false;
+	}
+
+};
+
+int main(){
+
+	return 0;
+}
