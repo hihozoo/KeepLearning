fddbd264837915bba41b12286d37a91cb92353d7
hihozoo <hihozoo@163.com>
2022-01-19 22:56:46 +0800

    二叉树的最大深度---层序遍历，最大深度就是最大层次

--- LeetCode/104-maximum-depth-of-binary-tree.cpp
+++ LeetCode/104-maximum-depth-of-binary-tree.cpp
@@ -8,6 +8,7 @@
 
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -27,4 +28,44 @@ public:
 		int right = maxDepth(root->right);
 		return (left > right ? left: right) + 1;
     }
+
+	int maxDepthDfs(TreeNode* root){
+		if (!root){
+			return 0;
+		}
+		int left = maxDepth(root->left);
+		int right = maxDepth(root->right);
+		return (left > right ? left: right) + 1;
+	}
+
+	// 层序遍历，广度优先
+	int maxDepthBfs(TreeNode* root){
+		if (!root){
+			return 0;
+		}
+
+		queue<TreeNode*> stk;
+		stk.push(root);
+
+		int depth = 0;
+		while (!stk.empty()){
+			int sz = stk.size();
+			while (sz > 0){
+				TreeNode* node = stk.front();
+				stk.pop();
+
+				if (node->left){
+					stk.push(node->left);
+				}
+				if (node->right){
+					stk.push(node->right);
+				}
+				sz -= 1;
+			}
+
+			depth += 1;
+		}
+		return depth;
+	}
+
 };
