0e91dd09b0215e4ff27d54b445a12af010b6ec4b
zhujisheng <zhujisheng@corp.netease.com>
2022-01-25 18:32:33 +0800

    二叉树的Z字形遍历

---
+++ LeetCode/103-binary-tree-zigzag-level-order-traversal.cpp
@@ -0,0 +1,46 @@
+/**
+ * 给你二叉树的根节点 root ，返回其节点值的 锯齿形层序遍历 。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+*/
+#include <stack>
+#include <vector>
+#include <iostream>
+#include <queue>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+struct TreeNode{
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+		vector<vector<int> > ret;
+		if (!root){
+			return ret;
+		}
+		deque<TreeNode*> deq;
+		deq.push_back(root);
+
+		bool isOrderLeft = true;
+
+		while (!deq.empty()){
+			int size = deq.size();
+			deque<int> levelList;
+			while (size-- > 0){
+				TreeNode* node = deq.front();
+				if (isOrderLeft){
+				}
+			}
+		}
+		return vec;
+	}
+};
