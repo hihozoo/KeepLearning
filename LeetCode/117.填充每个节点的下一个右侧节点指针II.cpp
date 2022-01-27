d415e7a5ae8fa2e24273e92ac73530cc3f4242e9
zhujisheng <zhujisheng@corp.netease.com>
2022-01-27 17:20:23 +0800

    117.填充每个节点的下一个右侧节点指针 II

---
+++ LeetCode/117.填充每个节点的下一个右侧节点指针II.cpp
@@ -0,0 +1,74 @@
+/**
+ * 给定一个二叉树
+
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+
+初始状态下，所有 next 指针都被设置为 NULL。
+
+ 
+
+进阶：
+
+你只能使用常量级额外空间。
+使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node-ii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <queue>
+
+using namespace std;
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+		if (root){
+			queue<Node*> que;
+			que.push(root);
+			while (!que.empty()){
+				int size = que.size();
+				Node* head = nullptr;
+				while (size-- > 0){
+					Node* cur = que.front();
+					que.pop();
+
+					if (head){
+						head->next = cur;
+					}
+					head = cur;
+
+					if (cur->left){
+						que.push(cur->left);
+					}
+					if (cur->right){
+						que.push(cur->right);
+					}
+				}
+			}
+		}
+		return root;
+	}
+};
