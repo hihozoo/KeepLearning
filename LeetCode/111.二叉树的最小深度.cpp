1694e9f52329e056f3f77e1ea112d38a9e899f7f
zhujisheng <zhujisheng@corp.netease.com>
2022-01-27 10:16:10 +0800

    二叉树的最小深度: 修改算法

--- LeetCode/111.二叉树的最小深度.cpp
+++ LeetCode/111.二叉树的最小深度.cpp
@@ -25,7 +25,7 @@ public:
 		}
 		int left = minDepth(root->left);
 		int right = minDepth(root->right);
-		if ((!left && right) || (left && !right)){
+		if ((left == 0 || right == 0) && left != right){
 			return max(left, right) + 1;
 		}
 		return min(left, right) + 1;
