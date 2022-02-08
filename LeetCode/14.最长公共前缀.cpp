8f99323d9bb26e74bac452ab67344062242286ef
zhujisheng <zhujisheng@corp.netease.com>
2022-02-08 15:05:42 +0800

    14最长公共前缀

--- LeetCode/14.最长公共前缀.cpp
+++ LeetCode/14.最长公共前缀.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -27,5 +28,29 @@ public:
 			i++;
 		}
 		return c1.substr(0, i);
+	}
+
+	string longestCommonPrefix2(vector<string>& strs){
+		if (strs.size() <= 0){
+			return "";
+		}
+		string common = strs[0];
+		int z = common.length();
+		for (int i = 1, c = strs.size(); i < c; i++){
+			string& cur_str = strs[i];
+
+			int m = z > cur_str.length() ? cur_str.length(): z;
+			int j = 0;
+			while (j < m && common[j] == cur_str[j]){
+				j++;
+			}
+			if (j < z){
+				z = j;
+			}
+			if (z == 0){
+				break;
+			}
+		}
+		return common.substr(0, z);
 	}
 };
